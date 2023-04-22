c7f502bcfce259c560ec613ddf7736213ac2bb48
jacks <jacks@COLIN-PC>
2023-04-22 09:50:25 -0400

    Restructure so less space reqd

--- whisperer/Form1.cs
+++ whisperer/Form1.cs
@@ -434,7 +434,7 @@ namespace whisperer
             Action act = null;
             while (!quitq)
             {
-                if (whisperq.TryDequeue(out act))
+                while (!quitq && whisperq.TryDequeue(out act))
                     act.Invoke();
                 Thread.Sleep(1000);
             }
