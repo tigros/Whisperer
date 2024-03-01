c5403e25af7c62591fd2920e07d9e062b191e781
tigros <straightupandup@yahoo.com>
2024-03-01 06:06:45 -0500

    Update GPUmembyproc.cpp to support Radeon

--- GPUmembyproc/GPUmembyproc.cpp
+++ GPUmembyproc/GPUmembyproc.cpp
@@ -38,7 +38,7 @@ bool GetDXGIAdapter(IDXGIAdapter **aDXGIAdapter)
         a2->GetDesc(&desc);
         if ((wcsstr(desc.Description, L"NVIDIA") || wcsstr(desc.Description, L"ATI") ||
             wcsstr(desc.Description, L"AMD") || wcsstr(desc.Description, L"Intel") ||
-	    wcsstr(desc.Description, L"Radeon")) && desc.DedicatedVideoMemory > lasttot)
+	        wcsstr(desc.Description, L"Radeon")) && desc.DedicatedVideoMemory > lasttot)
         {
             *aDXGIAdapter = a2;
             lasttot = totmem = desc.DedicatedVideoMemory;
